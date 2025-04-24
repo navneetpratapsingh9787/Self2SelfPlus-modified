b04d6489cb9b49a584ad2fcc137a317bee4ce41c
navneetpratapsingh9787 <97586444+navneetpratapsingh9787@users.noreply.github.com>
2025-04-24 17:20:41 +0530

    Update gatedConv.py

--- models/gatedConv.py
+++ models/gatedConv.py
@@ -1,22 +1,22 @@
 from torch import nn
 
 
-class GatedConv2d(nn.Module) :
-    def __init__(self, inChannels, outChannels, kernelSize, stride, padding) :
-        # Inheritance
+class GatedConv2d(nn.Module):
+    def __init__(self, inChannels, outChannels, kernelSize, stride, padding):
         super(GatedConv2d, self).__init__()
-        
-        # Create Convolution Layer Instance
-        self.conv = nn.Sequential(nn.Conv2d(inChannels, outChannels, kernelSize, stride, padding, padding_mode="reflect"),
-                                  nn.LeakyReLU(0.2))
-        self.maskedConv = nn.Sequential(nn.Conv2d(inChannels, outChannels, kernelSize, stride, padding, padding_mode="reflect"),
-                                        nn.Sigmoid())
+        self.conv = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(inChannels, outChannels, kernelSize, stride, padding, padding_mode="reflect")),
+            nn.LeakyReLU(0.2)
+        )
+        self.maskedConv = nn.Sequential(
+            nn.utils.spectral_norm(nn.Conv2d(inChannels, outChannels, kernelSize, stride, padding, padding_mode="reflect")),
+            nn.Sigmoid()
+        )
 
-    def forward(self, input) :
+    def forward(self, input):
         output = self.conv(input)
         outputMask = self.maskedConv(input)
-        
-        output = output*outputMask
-        
-        return output
+        return output * outputMask
+
+
 
