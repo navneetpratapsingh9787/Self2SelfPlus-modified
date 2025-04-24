b5da84b3087921cf73140aa0caa775bbc2eac2c3
navneetpratapsingh9787 <97586444+navneetpratapsingh9787@users.noreply.github.com>
2025-04-24 17:23:07 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,11 @@
 from torch import optim
-
 from tqdm import tqdm
-
 import config
 from data import dataloaders
 from models import models
 from utils import utils
 
-
-def main() : 
+def main():
     # Read Options
     opt = config.readArguments()
     
@@ -18,16 +15,16 @@ def main() :
     # Create Save Directory
     utils.mkdirs(imageSavePath)
     
-    for data in dataLoader :
-        # Create Model Instance
-        model = models.Self2SelfPlus(opt)
-        model = models.assignOnGpu(opt, model)
-        
-        # Create Optimizer Instance
-        optimizer = optim.Adam(model.net.parameters(), 
-                               lr=opt.lr, 
-                               betas=(opt.beta1, opt.beta2))
+    # Create Model Instance (Moved outside loop)
+    model = models.Self2SelfPlus(opt)
+    model = models.assignOnGpu(opt, model)
+    
+    # Create Optimizer Instance (Moved outside loop)
+    optimizer = optim.Adam(model.net.parameters(), 
+                          lr=opt.lr, 
+                          betas=(opt.beta1, opt.beta2))
 
+    for data in dataLoader:
         # Load Data
         noisyImage, name = data["noisyImage"], data["name"]
         
@@ -38,35 +35,35 @@ def main() :
         print("================================================================================================================================")
         print(f"< Image File Name : {name[0]} >")
     
-        with tqdm(total=opt.numIters) as pBar :
-            for iter in range(1, opt.numIters+1) :
+        with tqdm(total=opt.numIters) as pBar:
+            for iter in range(1, opt.numIters + 1):
                 # Train Denoising Autoencoder
                 optimizer.zero_grad()
                 loss = model(noisyImage, mode="train")
                 
-                # Compute Loss
+                # Compute Loss Components
                 lossSS = loss["Self-Supervised"].item()
                 lossIQA = loss["IQA"].item()
+                lossPerceptual = loss["Perceptual"].item()  # Added
                 
                 # Back-Propagation
-                loss = sum(loss.values()).mean()
-                loss.backward()
+                total_loss = sum(loss.values()).mean()
+                total_loss.backward()
                 optimizer.step()
 
                 # Show Training Procedure
-                pBar.set_description(desc=f"[{iter}/{opt.numIters}] < Loss(Self-Supervised):{lossSS:.8f} | Loss(IQA):{lossIQA:.8f} >")
+                pBar.set_description(desc=f"[{iter}/{opt.numIters}] < Loss(Self-Supervised):{lossSS:.8f} | Loss(IQA):{lossIQA:.8f} | Loss(Perceptual):{lossPerceptual:.8f} >")
                 pBar.update()
 
         # Get Inference Result
         denoisedImage = model(noisyImage, "inference")
         
         # Clamp Image
-        denoisedImage = denoisedImage.clamp(0,1)
+        denoisedImage = denoisedImage.clamp(0, 1)
         
         # Save Image
         utils.saveImage(denoisedImage, imageSavePath, name[0])
         print()
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
